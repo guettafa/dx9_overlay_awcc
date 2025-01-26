911583d8edc6e1c7cb73319d7b669718f22ef904
guettafa <irontitou5@gmail.com>
2025-01-26 15:14:26 -0500

    Update main.cpp

--- awcc_overlay/src/main.cpp
+++ awcc_overlay/src/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 	Render awccOverlay(
-		"HwndWrapper[AWCCOverlay;;b9650f5a-3293-4ae5-b2b3-26b99b4cbbb5]", // class name that correspond to disposable Handle from WPF ( AKA, a problem ) 
+		"HwndWrapper[AWCCOverlay;;b9650f5a-3293-4ae5-b2b3-26b99b4cbbb5]", // class name is generated dynamically
 		"AWCC Overlay",
 		WS_EX_LAYERED | WS_EX_TOOLWINDOW | WS_EX_TOPMOST | WS_EX_TRANSPARENT
 	);
