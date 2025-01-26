dcb4b36497d9dc8abff5e78bc7a30d0262919ab4
guettafa <irontitou5@gmail.com>
2025-01-26 13:48:39 -0500

    Changed D3D format from D3DFMT_UNKNOWN to D3DFMT_A8R8G8B8

--- awcc_overlay/src/main.cpp
+++ awcc_overlay/src/main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
 	Render awccOverlay(
-		"HwndWrapper[AWCCOverlay;;bf9c092f-66f9-4a17-b0ea-934dec865872]", // class name that correspond to disposable Handle from WPF ( AKA, a problem ) 
+		"HwndWrapper[AWCCOverlay;;b9650f5a-3293-4ae5-b2b3-26b99b4cbbb5]", // class name that correspond to disposable Handle from WPF ( AKA, a problem ) 
 		"AWCC Overlay",
 		WS_EX_LAYERED | WS_EX_TOOLWINDOW | WS_EX_TOPMOST | WS_EX_TRANSPARENT
 	);
--- awcc_overlay/src/render.cpp
+++ awcc_overlay/src/render.cpp
@@ -14,15 +14,13 @@ bool Render::CreateDevice()
 	ZeroMemory(&mParametersD3d9, sizeof(mParametersD3d9));
 
 	mParametersD3d9.Windowed				   = TRUE;
-	mParametersD3d9.BackBufferFormat	       = D3DFMT_UNKNOWN;
+	mParametersD3d9.BackBufferFormat	       = D3DFMT_A8R8G8B8;
 	mParametersD3d9.BackBufferCount			   = 1;
 	mParametersD3d9.EnableAutoDepthStencil     = TRUE;
 	mParametersD3d9.AutoDepthStencilFormat     = D3DFMT_D16;
 	mParametersD3d9.hDeviceWindow		       = mOverlay.mHandle;
 	mParametersD3d9.SwapEffect			       = D3DSWAPEFFECT_DISCARD;
 	mParametersD3d9.PresentationInterval       = D3DPRESENT_INTERVAL_ONE;
-
-	// May be D3DDEVTYPE_REF instead of ..HAL
 
 	HRESULT res = mD3d9->CreateDevice(
 		D3DADAPTER_DEFAULT,
@@ -84,10 +82,7 @@ bool Render::InitRendering()
 void Render::Loop(const std::function<void()>& func)
 {
 	bool over = false;
-	MSG msg{};
-
-	// ImGui Default bg Color
-	ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 0.0f);
+	MSG msg   = {};
 
 	while (!over)
 	{
@@ -123,10 +118,8 @@ void Render::Loop(const std::function<void()>& func)
 		mDeviceD3d9->SetRenderState(D3DRS_ZENABLE,		     FALSE);
 		mDeviceD3d9->SetRenderState(D3DRS_ALPHABLENDENABLE,  FALSE);
 		mDeviceD3d9->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
-
 		
-		D3DCOLOR clear_col_dx = D3DCOLOR_RGBA((int)(clear_color.x * clear_color.w * 255.0f), (int)(clear_color.y * clear_color.w * 255.0f), (int)(clear_color.z * clear_color.w * 255.0f), (int)(clear_color.w * 255.0f));
-		mDeviceD3d9->Clear(0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
+		mDeviceD3d9->Clear(0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_RGBA(0,0,0,0), 1.0f, 0);
 		if (mDeviceD3d9->BeginScene() >= 0)
 		{
 			ImGui::Render();
